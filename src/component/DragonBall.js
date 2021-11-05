0e9735887477d674eb277e8f869edfb25698906d
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-11-05 16:50:22 +0200

    add deleteHanler, input with use ref to fouces on element and ref insted of value

--- src/component/DragonBall.js
+++ src/component/DragonBall.js
@@ -1,16 +1,80 @@
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import DragonBallCaracther from "./DragonBallCaracther";
-import DragonBallData from "./DragonBallData";
+import DragonBallData from "./DragonBallData"; 
+
 
 export default function DragonBall() { 
 
-  const [dragonData, SetDragonData] = useState(DragonBallData);
+  const [dragonData, SetDragonData] = useState(DragonBallData);  
+
+  const [show,SetShow] = useState(!false)  
+
+  const foucesInput = useRef(null)
+
+//////////////////////////////////////////
 
 
+  const ShowHandler = () => {
+    
+      SetShow(!show)   
+      
+  }
 
-  return (
-    <div>
-     <DragonBallCaracther dragonData={dragonData}  /> 
+/////////////////////////////////////
+
+ 
+   const deletHandler = (e,clikedIdx) => { /// I change the pram name to clicked index to make differnt as programing // 
+
+    console.log(clikedIdx); 
+
+    const DeletImag = dragonData.filter((dragon,idx) => idx !== clikedIdx )   
+
+    SetDragonData(DeletImag)   
+
+    console.log(dragonData);  
+    console.log(DeletImag);  
+
+    ///////////////// Another way  
+
+    // SetDragonData ((prevState) => { /// setstate as  a Arrow functions  
+
+    //  return  prevState.filter((el,idx) => idx !== clikedIdx )
+      
+    // })  
+   }  
+
+   const foucsHandler = () => {
+
+        console.log(foucesInput.current.focus());
+        
+   } 
+
+   const toggleHandler = () => {
+
+    console.log(foucesInput.current.value); 
+    
+} 
+
+
+  
+
+  return (    
+
+   
+    <div> 
+
+       <button name='butoon' onClick={ShowHandler}> Show / Hide  Dragon ball Charcter  </button>   
+       <br />
+       <br /> 
+     <input type="text" placeholder='charcter Name'  ref ={foucesInput} onChange={toggleHandler} /> 
+
+     <button onClick={foucsHandler}> click to fouces on form  </button>  
+      { show &&  
+     <DragonBallCaracther dragonData={dragonData} deletHandler={deletHandler}  /> 
+      } 
+     
     </div>
   );
 }
+ 
+
--- src/component/DragonBallCaracther.js
+++ src/component/DragonBallCaracther.js
@@ -2,21 +2,31 @@ import React from "react";
 import Card from "react-bootstrap/Card"; 
 import './DragonCaracter.styles.css'
 
-export default function DragonBallCaracther({ dragonData }) {
+export default function DragonBallCaracther({ dragonData,deletHandler }) {
   return ( 
 
-    <div className='dragon'>
+    <div>
+        
 
-      {dragonData.map(({ Name, Imag }, idx) => (
-        <div key={idx}>
-          <Card style={{ width: "20px", hight: "20px" }}>
-            <h1> {Name}</h1>
+      {dragonData.map(({ Name, Imag }, idx) => ( 
+     
+  
+
+        <div key={idx}  className='dragon' > 
+
+        
+
+          <Card style={{ width: "20px", hight: "20px" }}> 
+
+
+            <h1 onClick={(e)=> deletHandler(e,idx)}> <span className='delet'> Delete  </span>   {Name} </h1>
             <img className='dragon-Imge' style={{ width: "250px", hight: "250px" }} src={Imag} />  
             <Card.Body></Card.Body>
           </Card>
 
-        </div>
-      ))}
+        </div> 
+      ))} 
+     
     </div> 
   );
 }
